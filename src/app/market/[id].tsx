19c033450ae739309b46a04709de9b7b903996e6
Márcio <marcionitao@gmail.com>
2025-01-20 22:40:23 +0100

    defined styles for market details to page from :id

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react'
-import { router, useLocalSearchParams } from 'expo-router'
-import { View, Text, Alert } from 'react-native'
+import { router, useLocalSearchParams, Redirect } from 'expo-router'
+import { View, Alert } from 'react-native'
 
 import { Loading } from '@/components/loading'
+import { Cover } from '@/components/market/cover'
+import { PropsDetails, Details } from '@/components/market/details'
 
 import { api } from '@/services/api'
-import { isLoading } from 'expo-font'
+
+type DataProps = PropsDetails & {
+  cover: string
+}
 
 export default function Market() {
   // criando um estado para armazenar os dados do local
-  const [data, setData] = useState()
+  const [data, setData] = useState<DataProps>()
   const [isLoading, setLoading] = useState(true)
   // recuperando o id da rota
   const params = useLocalSearchParams<{ id: string }>()
@@ -37,9 +42,15 @@ export default function Market() {
     return <Loading />
   }
 
+  if (!data) {
+    return <Redirect href="/home" />
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Market {params.id}</Text>
+    <View style={{ flex: 1 }}>
+      <Cover uri={data.cover} />
+
+      <Details data={data} />
     </View>
   )
 }
