a7df594e461eb1ba1f4d89293ed457b81fc22f3e
Márcio <marcionitao@gmail.com>
2025-01-20 17:12:21 +0100

    get each market details to page from :id

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,9 +1,42 @@
-import { useLocalSearchParams } from 'expo-router'
-import { View, Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import { router, useLocalSearchParams } from 'expo-router'
+import { View, Text, Alert } from 'react-native'
+
+import { Loading } from '@/components/loading'
+
+import { api } from '@/services/api'
+import { isLoading } from 'expo-font'
 
 export default function Market() {
+  // criando um estado para armazenar os dados do local
+  const [data, setData] = useState()
+  const [isLoading, setLoading] = useState(true)
   // recuperando o id da rota
   const params = useLocalSearchParams<{ id: string }>()
+  // função para buscar os dados do local
+  async function fetchMarket() {
+    try {
+      // buscando os dados do local
+      const { data } = await api.get('/markets/' + params.id)
+      setData(data)
+      // depois que os dados foram carregados, mudamos o estado de isLoading para false
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Locations', 'could not load locations', [
+        { text: 'OK', onPress: () => router.back() },
+      ])
+    }
+  }
+
+  useEffect(() => {
+    fetchMarket()
+  }, [params.id])
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Market {params.id}</Text>
