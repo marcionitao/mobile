282a8362d53f56279040c19f3c02847448f77765
Márcio <marcionitao@gmail.com>
2025-01-21 23:01:21 +0100

    add info component with details data

--- src/components/market/details/index.tsx
+++ src/components/market/details/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from 'react-native'
 import { IconPhone, IconMapPin, IconTicket } from '@tabler/icons-react-native'
 import { s } from './styles'
+import { Info } from '../info'
 
 export type PropsDetails = {
   name: string
@@ -25,6 +26,21 @@ export function Details({ data }: Props) {
       <Text style={s.description}>{data.description}</Text>
       <View style={s.group}>
         <Text style={s.title}>Informations</Text>
+        <Info
+          icon={IconTicket}
+          description={`${data.coupons} coupons available`}
+        />
+        <Info icon={IconMapPin} description={data.address} />
+        <Info icon={IconPhone} description={data.phone} />
+      </View>
+
+      <View style={s.group}>
+        <Text style={s.title}>Regulations</Text>
+        {data.rules.map((item) => (
+          <Text key={item.id} style={s.rule}>
+            {`\u2022 ${item.description}`}
+          </Text>
+        ))}
       </View>
     </View>
   )
